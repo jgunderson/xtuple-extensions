33818f6b4f77d1eb531d68f00a4d6e1ce51e045e
jgunderson <jgunderson@erpbisolutions.com>
2014-10-06 15:34:20 -0400

    fix manifest name

--- source/xtuple-radar/database/source/manifest.js
+++ source/xtuple-radar/database/source/manifest.js
@@ -1,1 +1,1 @@
-{  "name": "bi_crm_radar",  "version": "4.6.0",  "comment": "Business Intelligence Extensiion for CRM Radar Charts",  "loadOrder": 50,  "dependencies": ["bi_open"]}
+{  "name": "xtuple-radar",  "version": "4.6.0",  "comment": "Business Intelligence Extensiion for CRM Radar Charts",  "loadOrder": 50,  "dependencies": ["bi_open"]}
